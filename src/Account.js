2fb89a103a2a90c9885f19ad2fecc67d9b3529d4
Lillian Chernin <l.smith1654@gmail.com>
2018-01-29 16:41:58 -0600

    add zero class when balance is zero

--- src/Account.js
+++ src/Account.js
@@ -12,9 +12,13 @@ export default class Account extends Component {
     let currentValueAsNumber = Number(this.inputBox.value);
     let previousBalanceAsNumber = Number(this.state.balance);
     if (!(isNaN(currentValueAsNumber))) {
-      this.setState({
-        balance: previousBalanceAsNumber + currentValueAsNumber
-      })
+      if (currentValueAsNumber < 0) {
+        alert('Must enter a positive number!');
+      } else {
+        this.setState({
+          balance: previousBalanceAsNumber + currentValueAsNumber
+        })
+      }
     }
     this.inputBox.value = "";
   }
@@ -22,23 +26,32 @@ export default class Account extends Component {
     let currentValueAsNumber = Number(this.inputBox.value);
     let previousBalanceAsNumber = Number(this.state.balance);
     if (!(isNaN(currentValueAsNumber))) {
-      if (currentValueAsNumber > previousBalanceAsNumber) {
-        this.setState({
-          balance: 0
-        })
+      if (currentValueAsNumber < 0) {
+      alert('Must enter a positive number!');
       } else {
-        this.setState({
-          balance: previousBalanceAsNumber - currentValueAsNumber
-        })
+        if (currentValueAsNumber > previousBalanceAsNumber) {
+          alert('insufficient funds!');
+          this.setState({
+            balance: 0
+          })
+        } else {
+          this.setState({
+            balance: previousBalanceAsNumber - currentValueAsNumber
+          })
+        }
       }
     }
     this.inputBox.value = "";
   }
   render() {
+    let balanceClass = 'balance';
+    if (this.state.balance === 0) {
+      balanceClass += ' zero';
+    }
     return (
       <div className="account">
         <h2>{this.props.name}</h2>
-        <div className="balance">${this.state.balance}</div>
+        <div className={balanceClass}>${this.state.balance}</div>
         <input type="text" placeholder="enter an amount" ref={(input) => this.inputBox = input}/>
         <input onClick={(e) => this.handleDeposit(e)} type="button" value="Deposit" />
         <input onClick={(e) => this.handleWithdraw(e)} type="button" value="Withdraw" />
